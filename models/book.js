6a52555b939b813a4d940941a2ab24b674b632fb
ManuelGF <manuelg139@gmail.com>
2021-05-29 21:53:41 -0500

    Added Component Files, Added Pages to App.js and  Fixed Schema

--- models/book.js
+++ models/book.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  description: String,
-  image: String,
-  link: String
+    _id: { type: String },
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    synopsis: String,
+    date: { type: Date, default: Date.now },
+    image: { type: String },
+    link: { type: String }
 });
 
 const Book = mongoose.model("Book", bookSchema);
