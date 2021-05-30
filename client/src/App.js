4fb802d03e151fadffb6fb16ad70b145dbf7eb77
ManuelGF <manuelg139@gmail.com>
2021-05-30 08:49:42 -0500

    Added Results index.js for Search Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
